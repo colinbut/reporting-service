b91b2fe7447d8684277a101bbba006655bf8b583
Colin But <colinbut@users.noreply.github.com>
2018-08-03 09:38:43 +0100

    stub class for writing csv

---
+++ src/main/java/com/mycompany/reporting/csv/CSVWriter.java
@@ -0,0 +1,19 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.reporting.csv;
+
+import com.mycompany.reporting.format.ReportObject;
+
+public final class CSVWriter {
+
+    private CSVWriter() {
+    }
+
+    public static void writeToCSV(ReportObject reportObject) {
+        // TODO
+    }
+
+}
